4a7bc10ee03633929eaf2509a66eabff2d317a98
mishragini <mishragini8@gmail.com>
2026-03-31 08:51:38 +0530

    feat: add react router and shadcn

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { Login } from "./_components/Login";
 import "./App.css";
+import { Routes, Route } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>Dashboard</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ dashboard/src/_components/Login.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export function Login() {
+  return (
+    <>
+      <Button>Login with Github</Button>
+    </>
+  );
+}
